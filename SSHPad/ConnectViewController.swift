bf31470289758434f8244594fccdb84c0d47570e
Tomás Martins <tomas.marti@me.com>
2021-08-26 23:18:33 -0300

    Connect view controller layout update, splash screen and created support files directory

--- SSHPad/ConnectViewController.swift
+++ SSHPad/ConnectViewController.swift
@@ -9,14 +9,15 @@ import UIKit
 
 class ConnectViewController: UIViewController {
     
+    // MARK: - Components
     var stackView: UIStackView!
     var fieldsStackView: UIStackView!
     var usernameTextField: UITextField!
     var ipTextField: UITextField!
     var passwordTextField: UITextField!
-    
     var loginButton: UIButton!
 
+    // MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupNavigation()
@@ -24,31 +25,11 @@ class ConnectViewController: UIViewController {
         self.setupUI()
     }
     
-    @objc func login() {
-        let session = NMSSHSession(host: ipTextField.text!, andUsername: usernameTextField.text!)
-        session.connect()
-        if session.isConnected {
-            session.authenticate(byPassword: passwordTextField.text!)
-            
-            if session.isAuthorized {
-                print("Authorized!")
-            }
-        }
-        
-        var error: NSError?
-        let response: String = session.channel.execute("osascript Documents/SSHPad/terminal.scpt", error: &error)
-        print(response)
-        session.disconnect()
-    }
-    
+    // MARK: - UI Setup methods
     func setupNavigation() {
         self.title = "Sign in"
         self.view.backgroundColor = .white
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(didTapCancel))
-    }
-    
-    @objc func didTapCancel() {
-        self.dismiss(animated: true, completion: nil)
     }
     
     func setupComponents() {
@@ -72,7 +53,7 @@ class ConnectViewController: UIViewController {
         passwordTextField.translatesAutoresizingMaskIntoConstraints = false
         
         loginButton = UIButton()
-        loginButton.setTitle("Sign in with SSH", for: .normal)
+        loginButton.setTitle("Sign in via SSH", for: .normal)
         loginButton.backgroundColor = UIColor(red: 0.14, green: 0.31, blue: 0.67, alpha: 1.00)
         loginButton.tintColor = .white
         loginButton.addTarget(self, action: #selector(login), for: .touchUpInside)
@@ -85,12 +66,21 @@ class ConnectViewController: UIViewController {
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        fieldsStackView = UIStackView()
+        fieldsStackView.axis = .vertical
+        fieldsStackView.alignment = .fill
+        fieldsStackView.distribution = .equalSpacing
+        fieldsStackView.isLayoutMarginsRelativeArrangement = true
+        fieldsStackView.spacing = 24
+        fieldsStackView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func setupUI() {
-        stackView.addArrangedSubview(usernameTextField)
-        stackView.addArrangedSubview(ipTextField)
-        stackView.addArrangedSubview(passwordTextField)
+        fieldsStackView.addArrangedSubview(usernameTextField)
+        fieldsStackView.addArrangedSubview(ipTextField)
+        fieldsStackView.addArrangedSubview(passwordTextField)
+        stackView.addArrangedSubview(fieldsStackView)
         stackView.addArrangedSubview(loginButton)
         self.view.addSubview(stackView)
         
@@ -98,6 +88,28 @@ class ConnectViewController: UIViewController {
         stackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
         stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         stackView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    // MARK: - Gesture methods
+    @objc func login() {
+        let session = NMSSHSession(host: ipTextField.text!, andUsername: usernameTextField.text!)
+        session.connect()
+        if session.isConnected {
+            session.authenticate(byPassword: passwordTextField.text!)
+            
+            if session.isAuthorized {
+                print("Authorized!")
+            }
+        }
+        
+        var error: NSError?
+        let response: String = session.channel.execute("osascript Documents/SSHPad/terminal.scpt", error: &error)
+        print(response)
+        session.disconnect()
+    }
+    
+    @objc func didTapCancel() {
+        self.dismiss(animated: true, completion: nil)
     }
 
 }
