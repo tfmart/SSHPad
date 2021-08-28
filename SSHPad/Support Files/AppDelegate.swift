e9b27b288e08986e05f2850653c4e17c5a0dfcc6
Tomás Martins <tomas.martins@guru.com.vc>
2021-08-28 17:14:06 -0300

    iOS 13+ fix

--- SSHPad/Support Files/AppDelegate.swift
+++ SSHPad/Support Files/AppDelegate.swift
@@ -33,15 +33,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    @available(iOS 13.0, *)
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-      
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UINavigationController(rootViewController: ViewController())
-        window.makeKeyAndVisible()
-        self.window = window
-    }
 }
 
