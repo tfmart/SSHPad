f0ad2cba1ed7fec32a1616d309ee6a7124ffa018
Tomás Martins <tomas.marti@me.com>
2021-09-09 22:17:51 -0300

    Script cell animation and styling

--- SSHPad/Gallery/Components/ScriptCollectionViewCell.swift
+++ SSHPad/Gallery/Components/ScriptCollectionViewCell.swift
@@ -16,6 +16,12 @@ class ScriptCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    override var isHighlighted: Bool {
+        didSet {
+            toggleAnimation()
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
         self.setupComponents()
@@ -26,14 +32,31 @@ class ScriptCollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupComponents() {
+    private func setupComponents() {
         self.label = UILabel()
+        self.layer.cornerRadius = 8.0
         self.label.translatesAutoresizingMaskIntoConstraints = false
     }
     
-    func setupUI() {
+    private func setupUI() {
         contentView.addSubview(label)
         label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+    }
+    
+    private func toggleAnimation() {
+        if isHighlighted {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 5, options: .curveEaseInOut, animations: {
+                            self.alpha = 0.8
+                self.transform = CGAffineTransform.identity.scaledBy(x: 0.9, y: 0.9)
+            })
+        } else {
+            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) {
+                self.alpha = 1.0
+                self.transform = CGAffineTransform.identity
+            }
+        }
+        
     }
 }
